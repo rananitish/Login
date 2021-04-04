0b4f1d0c333bff7f760609070488d720222cac44
Nitish Rana <nitishrana@Nitishs-MacBook-Air.local>
2021-04-05 02:46:48 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="loginpage">
+      <div className="dine-overlay">
+              <div className="automate-takeaway">
+              <b>Automate your takeaway</b> </div>
+               <p><b>Pos features help your business to be fully automated with <br/> DineOrder online ordering system.</b></p>
+               <a href="#" class="login-box" tabindex="0">
+							<b>View More</b>
+						</a>
+       </div>
+    <div className="log-section">
+      <div className="log-details">
+        <form id ="loginForm" action="" method="post">
+         <div className="form-element-wrapper">
+           <input className='form-input' autoComplete='off' name='login_user_Username' type="email" name="" id="email" placeHolder="Email Address or Phone Number"/>
+           <label htmlFor="email address" className="form-label">Email Address or Phone Number</label>
+         </div>
+         <div className="form-element-wrapper">
+           <input type="password" name="login_user_Password" id="login-password" autoComplete="off" className="form-input" placeholder="Password"   />
+           <label htmlFor="pos_customer_Username" className="form-label">Password</label>
+         </div>
+         <a href="#" className="edit-receipt-btn"><b>LOG IN</b></a>
+       </form>
+      </div>
+     </div>
+   </div>
   );
 }
 
