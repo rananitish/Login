c521e87bfd7578eeafade848f8f748cdc23eb59d
Nitish Rana <nitishrana@Nitishs-MacBook-Air.local>
2021-10-26 23:18:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
       <div className="log-details">
         <form id ="loginForm" action="" method="post">
          <div className="form-element-wrapper">
-           <input className='form-input' autoComplete='off' name='login_user_Username' type="email" name="" id="email" placeHolder="Email Address or Phone Number"/>
+           <input className='form-input' autoComplete='on' name='login_user_Username' type="email" name="" id="email" placeHolder="Email Address or Phone Number"/>
            <label htmlFor="email address" className="form-label">Email Address or Phone Number</label>
          </div>
          <div className="form-element-wrapper">
-           <input type="password" name="login_user_Password" id="login-password" autoComplete="off" className="form-input" placeholder="Password"   />
+           <input type="password" name="login_user_Password" id="login-password" autoComplete="on" className="form-input" placeholder="Password"   />
            <label htmlFor="pos_customer_Username" className="form-label">Password</label>
          </div>
          <a href="#" className="edit-receipt-btn"><b>LOG IN</b></a>
